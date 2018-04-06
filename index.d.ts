a6794f441645d8bd763440d934815605ef0264a6
Evgeny Stulnikov <xxjenekxx@gmail.com>
2018-04-06 16:39:25 +0300

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,6 @@
 declare module "browser-passworder" {
   import { Buffer } from "buffer";
 
-  function encrypt(password: string, privateKey: Buffer): Promise<string>;
-  function decrypt(password: string, encrypted: string): Promise<Buffer>
-
-  export = {
-    encrypt,
-    decrypt
-  }
+  export function encrypt(password: string, privateKey: Buffer): Promise<string>;
+  export function decrypt(password: string, encrypted: string): Promise<Buffer>
 }
